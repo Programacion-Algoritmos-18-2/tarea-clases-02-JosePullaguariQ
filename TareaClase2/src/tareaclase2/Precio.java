d4c1f3f2b70814a1279f561fec9b4b6673fe487e
Jose Pullaguari <jepullaguari@utpl.edu.ec>
2018-10-12 10:30:15 -0500

    subiendo

---
+++ TareaClase2/src/tareaclase2/Precio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Precio {
+
+    double euros;
+
+    public double obtener(){
+        return 0;
+    }
+    public void agregar(double x){
+
+    }
+}
---
+++ TareaClase2/src/tareaclase2/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Principal {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Precio p;
+        p=new Precio();
+        System.out.println(p.euros);
+    }
+    
+}
